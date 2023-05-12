ab27ab57701ae71b87c00f42413f7a4e6cb8ac6f
gaole <glghkkll@163.com>
2023-05-12 15:42:07 +0800

    添加文字逐行打印效果

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
+// 改变 Nav 样式
 const nav = document.querySelector('.nav')
-const aList = document.querySelectorAll('.nav li a')
-
 window.addEventListener('scroll', fixNav)
 
 function fixNav() {
-    if(window.scrollY > nav.offsetHeight) {
+    if (window.scrollY > nav.offsetHeight) {
         nav.classList.add('active')
     } else {
         nav.classList.remove('active')
     }
 }
 
+// 点击 nav 高亮显示
+const aList = document.querySelectorAll('.nav li a')
 aList.forEach(item => {
     item.addEventListener('click', () => {
         removeAllCurrent()
         item.classList.add('current')
     })
 })
-
 function removeAllCurrent() {
     aList.forEach(item => {
         item.classList.remove('current')
     })
+}
+
+// 逐字打印效果
+const textEl = document.querySelector('#autotext')
+const text = "欢迎来到我的个人网站"
+let idx = 1
+writeText()
+function writeText() {
+    textEl.innerText = text.slice(0, idx)
+    idx++
+    if (idx > text.length) {
+        return
+    }
+    setTimeout(() => {
+        writeText()
+    }, 300);
+
 }
