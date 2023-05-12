64b53c7771088bc4173be7b10bfb020bd6849647
gaole <glghkkll@163.com>
2023-05-12 14:35:18 +0800

    add footer

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const nav = document.querySelector('.nav')
+const aList = document.querySelectorAll('.nav li a')
 
 window.addEventListener('scroll', fixNav)
 
 function fixNav() {
-    if(window.scrollY > nav.offsetHeight + 150) {
+    if(window.scrollY > nav.offsetHeight) {
         nav.classList.add('active')
     } else {
         nav.classList.remove('active')
     }
+}
+
+aList.forEach(item => {
+    item.addEventListener('click', () => {
+        removeAllCurrent()
+        item.classList.add('current')
+    })
+})
+
+function removeAllCurrent() {
+    aList.forEach(item => {
+        item.classList.remove('current')
+    })
 }
