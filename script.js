ba73f64c6c556e681b182b1ca24a23b9a404caa0
gaole <glghkkll@163.com>
2023-06-20 12:06:33 +0800

    优化明暗切换，提高js代码可读性

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 // 改变 Nav 样式
-const nav = document.querySelector('.nav')
-window.addEventListener('scroll', fixNav)
+const nav = document.querySelector('.nav');
+window.addEventListener('scroll', fixNav);
 
 function fixNav() {
-    if (window.scrollY > nav.offsetHeight) {
-        nav.classList.add('active')
+    const scrollOffset = nav.offsetHeight;
+    if (window.scrollY > scrollOffset) {
+        nav.classList.add('active');
     } else {
-        nav.classList.remove('active')
+        nav.classList.remove('active');
     }
 }
 
 // 逐字打印效果
-const textEl = document.querySelector('#autotext')
-const text = "欢迎来到我的个人网站"
+const textEl = document.querySelector('#autotext');
+const text = "欢迎来到我的个人网站";
 let idx = 1
 writeText()
 function writeText() {
@@ -24,33 +25,31 @@ function writeText() {
     setTimeout(() => {
         writeText()
     }, 300);
-
 }
 
 // 改变明暗显示模式
-const mode = document.querySelector('.mode')
+const mode = document.querySelector('.mode');
+const html = document.querySelector('html');
+mode.addEventListener('click', toggleDarkMode);
 
-mode.addEventListener('click', () => {
-    const html = document.querySelector('html')
-
-    if (html.classList.contains('dark')) {
-        html.classList.remove('dark')
-    } else {
-        html.classList.add('dark')
-    }
-})
-
-// 添加名言警句
-const quoteEl = document.querySelector('#quote')
-async function getQuote() {
-    const res = await fetch('https://v1.hitokoto.cn?c=d&c=k')
-    const data = await res.json()
-    console.log(data);
-    quoteEl.innerText = `${data.hitokoto}——${data.from_who == null ? '佚名' : data.from_who}`
+function toggleDarkMode() {
+    html.classList.toggle('dark');
 }
 
-getQuote()
-const autoTextEl = document.querySelector('.autotext')
-autoTextEl.addEventListener('click', () => {
-    getQuote()
-})
+// 添加名言警句
+const quoteEl = document.querySelector('#quote');
+const autoTextEl = document.querySelector('.autotext');
+getQuote();
+
+async function getQuote() {
+    try {
+        const response = await fetch('https://v1.hitokoto.cn?c=d&c=k');
+        const data = await response.json();
+        const quote = `${ data.hitokoto } —— ${ data.from_who || '佚名'}`
+        quoteEl.textContent = quote;
+    } catch (error) {
+        console.error('获取名言警句时出现错误:', error);
+    }
+}
+
+autoTextEl.addEventListener('click', getQuote);
